fbaaa660183b6b1cd4f1d59bb80f2f7eba0c0b61
Artem <Artemalex1990@gmail.com>
2016-12-06 19:11:07 +0200

    формула для того чтоб найти длинну массива и в случае пустого массива выдать смс с текстом: var i=0;
       if (arr.length==i) return ('its clear bro')
    и

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-﻿
+var arr=(7,8,9,5,1);
+function min(arr) 
+{
+var i=0;
+  
+   if (arr.length==i) return ('its clear bro')
+
+else
+
+ return Math.min.apply(null, arr); 
+}
+
+
+
+console.log(min([2])); 
