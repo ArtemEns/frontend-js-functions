86aa6f8ca595e0871310fa279820827da0440a5c
Artem <Artemalex1990@gmail.com>
2016-12-08 14:16:30 +0200

    работают три функции, меньшее число, игнор строк, пустая строка-текст

--- src/index.js
+++ src/index.js
@@ -1,18 +1,16 @@
-var arr=(7,8,9,5,1);
-function min(arr) 
+var arr=[7,8,9,5,'cca',1];
+
+function min(arr)
 {
+arr = arr.filter(Number);
+ 
 var i=0;
   
    if (arr.length==i) return ('its clear bro')
 
 else
 
- return Math.min.apply(null, arr); 
+return Math.min.apply(null, arr); 
 }
 
-
-
-console.log(min([2])); 
-
-
-
+console.log(min([3,12,4,'t'])); 
