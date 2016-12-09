208b4dfc5d0f0c986c92326a54bb0696dad21a3b
Artem <Artemalex1990@gmail.com>
2016-12-09 23:45:32 +0200

    функция минимума работает с нулем

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
+
 var arr=[7,8,9,5,'cca',1];
 
 function min(arr)
 {
-arr = arr.filter(Number);
- 
+arr = arr.filter(function(value){return typeof value==='number';})
+
 var i=0;
   
    if (arr.length==i) return ('its clear bro')
 
 else
 
-return Math.min.apply(null, arr); 
+return Math.min.apply(null, arr);
 }
 
-console.log(min([3,12,4,'t'])); 
+console.log(min([45,67,78,0,'tv',true])); 
