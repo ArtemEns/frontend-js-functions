b77df9fbdd188dbaf10e4e246340972a71eaaef2
Artem <Artemalex1990@gmail.com>
2016-12-11 16:16:52 +0200

    работающий, идеальный, прекрасный, как нарисованный код... автор -Артем Присяжный
    :
    посл изменения:
    вычисляет сумму из массива игнорируя строки, и булеан, если массив пуст выдает смс с текстом, и это прекрасно..

--- src/index.js
+++ src/index.js
@@ -15,3 +15,73 @@ return Math.min.apply(null, arr);
 }
 
 console.log(min([45,67,78,0,'tv',true])); 
+
+
+
+///////MAX/////////(same)
+
+
+
+
+var arr=[7,8,9,5,'cca',1];
+
+function max(arr)
+{
+arr = arr.filter(function(value){return typeof value==='number';})
+
+var i=0;
+  
+   if (arr.length==i) return ('its clear bro')
+
+else
+
+return Math.max.apply(null, arr);
+}
+
+console.log(max([45,67,78,0,'tv',true])); 
+
+
+
+
+
+////////SUM////////////////
+
+
+
+
+
+
+
+var arr=[7,8,9,5,'cca',1];
+function summa(arr)
+{
+  arr = arr.filter(function(value){return typeof value==='number';})
+  
+  var i=0;
+  
+   if (arr.length==i) return ('its clear bro')
+
+else
+
+return arr.reduce(function(sum,current){
+  return sum+current;},0);
+  
+
+}
+console.log(summa([1,1,2,0,'tv',true])); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
