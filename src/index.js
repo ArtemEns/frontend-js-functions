93df613401e2da4ece8fe6ba80aed750cf7775cc
Artem <Artemalex1990@gmail.com>
2016-12-29 12:48:36 +0200

    тесты проходят

--- src/index.js
+++ src/index.js
@@ -1,49 +1,31 @@
-///////////MIN/////////////
-
-var arr = [7, 8, 9, 5, 'cca', 1];
-
-function min(arr) 
-{
-  arr = arr.filter(function (value) { return typeof value === 'number'; })
-  var i = 0;
-  if (arr.length == i) return (undefined)
+function min(arr) {
+  if (!arr || arr.length == 0)
+  { return }
   else
-    return Math.min.apply(null, arr);
+    arr = arr.filter(function (value) { return typeof value === 'number'; })
+  return Math.min.apply(null, arr);
 }
-console.log(min([null, false, "4", undefined, true, 99, 6]));
 
 
-//-------------------------------------------------------------------------------
-///////MAX/////////(same)
-
-var arr = [7, 8, 9, 5, 'cca', 1];
-
-function max(arr)
- {
-  arr = arr.filter(function (value) { return typeof value === 'number'; })
-  var i = 0;
-  if (arr.length == i) return (undefined)
+function max(arr) {
+  if (!arr || arr.length == 0)
+  { return }
   else
-    return Math.max.apply(null, arr);
+    arr = arr.filter(function (value) { return typeof value === 'number'; })
+  return Math.max.apply(null, arr);
 }
-console.log(max([null, false, "4", undefined, true, 99, 6]));
 
 
-//-------------------------------------------------------------------------------
-////////SUM////////////////
 
-var arr = [7, 8, 9, 5, 'cca', 1];
-function sum() 
-{
-      if (arguments.length === 0) { return undefined; }
-      var len = arguments.length;
-      var result = arguments[0];
-      for (var i = 1; i < len; i++)
-     {
-      var num = typeof (arguments[i]);
-      if (num != "number") { arguments[i] = 0; }
-      result += arguments[i];
-     }
+function sum() {
+  if (arguments.length === 0) { return 0; }
+  var len = arguments.length;
+  var result = arguments[0];
+  for (var i = 1; i < len; i++) {
+    var num = typeof (arguments[i]);
+    if (num != "number") { arguments[i] = 0; }
+    result += arguments[i];
+  }
   return result;
 }
-console.log(sum(null, false, "4", undefined, true, 99, 6)); 
+
